cd67ec1e9c4683267a7657b0b51c62b59e477225
anakarina-caetano <197325078+anakarina-caetano@users.noreply.github.com>
2026-05-17 14:34:37 -0300

    Pratica 1 - Parte 4: Criação da tela de registro

---
+++ app/src/main/java/br/com/edu/weatherapp/RegisterActivity.kt
@@ -0,0 +1,132 @@
+package br.com.edu.weatherapp
+
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.edu.weatherapp.ui.theme.WeatherAppTheme
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RegisterPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RegisterPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordConfirm by rememberSaveable { mutableStateOf("") }
+    var user by rememberSaveable { mutableStateOf("") }
+    val activity = LocalActivity.current as Activity
+    Column(
+        modifier = modifier
+            .padding(24.dp)
+            .fillMaxSize()
+            .fillMaxWidth(fraction = 0.9f),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(
+            text = "Bem-vindo/a!",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = modifier.size(6.dp))
+        OutlinedTextField(
+            value = user,
+            label = { Text(text = "Digite seu usuário") },
+            modifier = modifier,
+            onValueChange = { user = it }
+        )
+        Spacer(modifier = modifier.size(12.dp))
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = modifier,
+            onValueChange = { email = it }
+        )
+        Spacer(modifier = modifier.size(12.dp))
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = modifier,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Spacer(modifier = modifier.size(12.dp))
+        OutlinedTextField(
+            value = passwordConfirm,
+            label = { Text(text = "Confirme a senha") },
+            modifier = modifier,
+            onValueChange = { passwordConfirm = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Row(
+            modifier = modifier.padding(12.dp).fillMaxSize(),
+            Arrangement.SpaceAround
+        ) {
+            Button(
+                onClick = {
+                    Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+                    activity.startActivity(
+                        Intent(activity, MainActivity::class.java).setFlags(
+                            FLAG_ACTIVITY_SINGLE_TOP
+                        )
+                    )
+                },
+                enabled = email.isNotEmpty() && password.isNotEmpty()
+            ) {
+                Text("Registrar")
+            }
+            Button(
+                onClick = { email = ""; password = "" },
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun RegisterPagePreview() {
+    WeatherAppTheme {
+        RegisterPage()
+    }
+}
